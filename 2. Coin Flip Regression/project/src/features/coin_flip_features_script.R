a02309f9b499e4384378c4cea86479a08505f4da
Qingyao Zhu <115422753+KatherineZ1225@users.noreply.github.com>
2022-11-12 01:58:29 -0500

    Delete 2. Coin Flip Regression directory

--- 2. Coin Flip Regression/project/src/features/coin_flip_features_script.R
+++
@@ -1,8 +0,0 @@
-library(data.table)
-
-DT<-fread('./project/volume/data/raw/train_file.csv')
-test <- fread('./project/volume/data/raw/test_file.csv')
-test$result <- -1
-
-fwrite(DT,'./project/volume/data/interim/train.csv')
-fwrite(test,'./project/volume/data/interim/test.csv')
